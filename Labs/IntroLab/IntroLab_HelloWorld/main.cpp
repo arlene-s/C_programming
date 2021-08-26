8e7a4d32a48c9e22267d78c403724fab5e396de5
arlene sagaoinit <asagaoinit1@student.rccd.edu>
2021-08-26 12:29:49 -0700

    Uploading intro lab

---
+++ Labs/IntroLab/IntroLab_HelloWorld/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Arlene Sagaoinit
+ * Created on August 26, 2021, 11:19 AM
+ * Purpose:  Run a simple "Hello World" program
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output Library
+using namespace std;  //STD Name-space where Library is compiled
+
+//User Libraries
+
+//Global Constants not Variables
+//Math/Physics/Science/Conversions/Dimensions
+
+//Function Prototypes
+
+//Code Begins Execution Here with function main
+int main(int argc, char** argv) {
+    //Set random number seed once here
+    
+    //Declare variables here
+    
+    //Initialize variables here
+    
+    //Map inputs to outputs here, i.e. the process
+    
+    //Display the results
+    cout<<"Hello World!" << endl;
+    return 0;
+}
